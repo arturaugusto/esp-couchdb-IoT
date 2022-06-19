93fb844c6c732da6d5d511881ebcb9bcbf5e4fb4
Artur Augusto Martins <arturaugusto@gmail.com>
2022-06-19 18:18:27 -0300

    update

--- app.js
+++ app.js
@@ -16,6 +16,42 @@ const dateRangeFlatPicker = flatpickr(document.getElementById('dateRange'), {
 })
 
 
+const matrixReducer = (a, c) => {
+  let t0 = idDateToTimestamp(c._id)
+  Object.keys(a).forEach(k => a[k].push(c[k]))
+  return a
+}
+
+
+const registerChart = (chartId, keys, timeMatrix, dataMap, uniqSensors) => {
+  let reducerAccumulatorObject = keys.reduce((a, c) => (a[c] = [])&&a, {})
+
+  let data = dataMap
+  .reduce(matrixReducer, reducerAccumulatorObject)
+
+  let xSize = timeMatrix.flat().length
+
+  let bufferMatrix = new Array(keys.length * uniqSensors.length).fill().map(_ => new Array(xSize).fill(null))
+  // ...
+
+
+  let flatSensorsMatrix = uniqSensors.map(sensor => {
+    return keys.map(k => data[k].flat())
+  }).flat()
+
+  let chart = new uPlot(CHARTS_OPTS[chartId], [
+      // timeMatrix.map(tArr => tArr[0]),
+      timeMatrix.map((tArr, i) => tArr.slice(0, data[keys[0]][i].length)).flat(),
+    ].concat(flatSensorsMatrix),
+    document.getElementById(chartId)
+  )
+
+  CHARTS.push({
+    id: chartId,
+    keys: keys,
+    chart: chart
+  })
+}
 
 const queryDateRange = () => {
   
@@ -28,17 +64,11 @@ const queryDateRange = () => {
       item.chart.destroy()
     })
 
-    let dataMap = data
-    .rows
-    .map(row => row.doc)
-    .filter(doc => doc._id[0] !== '_')
-    
-    let matrixReducer = (a, c) => {
-      let t0 = idDateToTimestamp(c._id)
-      Object.keys(a).forEach(k => a[k].push(c[k]))
-      return a
-    }
+    let dataMap = data.rows.map(row => row.doc)
 
+    let sensorArray = dataMap.map(doc => doc._id.split('_')[1])
+    let uniqSensors = Array.from(new Set(sensorArray))
+    
     ////////////////////////////////////////////
     // time
     let timeMatrix = dataMap.map(doc => {
@@ -49,40 +79,15 @@ const queryDateRange = () => {
 
     ////////////////////////////////////////////
     // temp
-    let tempHumData = dataMap
-    .reduce(matrixReducer, {'temp': [], 'hum': []})
-    
-    CHARTS.push({id: 'tempHum', keys: ['temp', 'hum'], chart: new uPlot(tempHumOpts, [
-      timeMatrix.map(tArr => tArr[0]),
-      tempHumData.temp.flat(),
-      tempHumData.hum.flat(),
-    ], document.getElementById('temp'))})
+    registerChart('tempHum', ['temp', 'hum'], timeMatrix, dataMap, uniqSensors)
     
     ////////////////////////////////////////////
     // Gy
-    let gyData = dataMap
-    .reduce(matrixReducer, {'GyX': [], 'GyY': [], 'GyZ': []})
-    
-    CHARTS.push({id: 'Gy', keys: ['GyX', 'GyY', 'GyZ'], chart: new uPlot(optsGy, [
-      timeMatrix.flat(),
-      gyData.GyX.flat(),
-      gyData.GyY.flat(),
-      gyData.GyZ.flat(),
-    ], document.getElementById('Gy'))})
-
+    registerChart('Gy', ['GyX', 'GyY', 'GyZ'], timeMatrix, dataMap, uniqSensors)
     
     ////////////////////////////////////////////
     // Ac
-    let acData = dataMap
-    .reduce(matrixReducer, {'AcX': [], 'AcY': [], 'AcZ': []})
-    
-    CHARTS.push({id: 'Ac', keys: ['AcX', 'AcY', 'AcZ'], chart: new uPlot(optsAc, [
-      timeMatrix.flat(),
-      acData.AcX.flat(),
-      acData.AcY.flat(),
-      acData.AcZ.flat(),
-    ], document.getElementById('Ac'))})
-
+    registerChart('Ac', ['AcX', 'AcY', 'AcZ'], timeMatrix, dataMap, uniqSensors)
 
   })
 }
--- chartOpts.js
+++ chartOpts.js
@@ -2,124 +2,124 @@
 const labelFormarter = (u, v) => new Date(v*1000).toString().split(' ').slice(1,5).join(' ')
 const xAxesFormarter = (u, vals) => vals.map(v => labelFormarter(undefined, v))
 
-const tempHumOpts = {
-  width: 920,
-  height: 400,
-  // cursor: cursorOpts,
-  scales: {
-    x: {
-      time: true,
+const CHARTS_OPTS = {
+  tempHum: {
+    width: 920,
+    height: 400,
+    // cursor: cursorOpts,
+    scales: {
+      x: {
+        time: true,
+      },
     },
+    series: [
+      {
+        value: labelFormarter,
+      },
+      {
+        scale: "°C",
+        label: "Temperatura",
+        stroke: "red",
+        value: (u, v, space) => v.toFixed(1) + " °C",
+        // fill: "rgba(255,0,0,0.1)",
+      },
+      {
+        scale: "%",
+        label: "Umidade",
+        stroke: "green",
+        value: (u, v, space) => v.toFixed(1) + " %",
+        // fill: "rgba(0,255,0,0.1)",
+      },
+    ],
+    axes: [
+      {},
+      {
+        scale: "°C",
+        label: "Temperatura",
+        size: 60,
+        values: (u, vals, space) => vals.map(v => +v.toFixed(1) + "°C"),
+      },
+      {
+        side: 1,
+        scale: "%",
+        label: "Umidade",
+        values: (u, vals, space) => vals.map(v => +v.toFixed(2) + " %"),
+        grid: {show: false},
+      },
+    ],
   },
-  series: [
-    {
-      value: labelFormarter,
+  Gy: {
+    width: 920,
+    height: 400,
+    // cursor: cursorOpts,
+    scales: {
+      x: {
+        time: true,
+      },
     },
-    {
-      scale: "°C",
-      label: "Temperatura",
-      stroke: "red",
-      value: (u, v, space) => v.toFixed(1) + " °C",
-      // fill: "rgba(255,0,0,0.1)",
-    },
-    {
-      scale: "%",
-      label: "Umidade",
-      stroke: "green",
-      value: (u, v, space) => v.toFixed(1) + " %",
-      // fill: "rgba(0,255,0,0.1)",
-    },
-  ],
-  axes: [
-    {},
-    {
-      scale: "°C",
-      label: "Temperatura",
-      size: 60,
-      values: (u, vals, space) => vals.map(v => +v.toFixed(1) + "°C"),
-    },
-    {
-      side: 1,
-      scale: "%",
-      label: "Umidade",
-      values: (u, vals, space) => vals.map(v => +v.toFixed(2) + " %"),
-      grid: {show: false},
-    },
-  ],
-};
-
-const optsGy = {
-  width: 920,
-  height: 400,
-  // cursor: cursorOpts,
-  scales: {
-    x: {
-      time: true,
-    },
+    series: [
+      {
+        value: labelFormarter,
+      },
+      {
+        stroke: "red",
+        label: "GyX",
+        // fill: "rgba(255,0,0,0.1)",
+      },
+      {
+        stroke: "green",
+        label: "GyY",
+        // fill: "rgba(0,255,0,0.1)",
+      },
+      {
+        stroke: "blue",
+        label: "GyZ",
+        // fill: "rgba(0,0,255,0.1)",
+      },
+    ],
+    axes: [
+      {},
+      {
+        label: "MPU read",
+        size: 60
+      },
+    ]
   },
-  series: [
-    {
-      value: labelFormarter,
+  Ac: {
+    width: 920,
+    height: 400,
+    // cursor: cursorOpts,
+    scales: {
+      x: {
+        time: true,
+      },
     },
-    {
-      stroke: "red",
-      label: "GyX",
-      // fill: "rgba(255,0,0,0.1)",
-    },
-    {
-      stroke: "green",
-      label: "GyY",
-      // fill: "rgba(0,255,0,0.1)",
-    },
-    {
-      stroke: "blue",
-      label: "GyZ",
-      // fill: "rgba(0,0,255,0.1)",
-    },
-  ],
-  axes: [
-    {},
-    {
-      label: "MPU read",
-      size: 60
-    },
-  ]
-};
-
-const optsAc = {
-  width: 920,
-  height: 400,
-  // cursor: cursorOpts,
-  scales: {
-    x: {
-      time: true,
-    },
-  },
-  series: [
-    {
-      value: labelFormarter,
-    },
-    {
-      stroke: "red",
-      label: "AcX",
-      // fill: "rgba(255,0,0,0.1)",
-    },
-    {
-      stroke: "green",
-      label: "AcY",
-      // fill: "rgba(0,255,0,0.1)",
-    },
-    {
-      stroke: "blue",
-      label: "AcZ",
-      // fill: "rgba(0,0,255,0.1)",
-    },
-  ],
-  axes: [
-    {},
-    {
-      label: "MPU read",
-      size: 60
-    },
-  ]
-};
+    series: [
+      {
+        value: labelFormarter,
+      },
+      {
+        stroke: "red",
+        label: "AcX",
+        // fill: "rgba(255,0,0,0.1)",
+      },
+      {
+        stroke: "green",
+        label: "AcY",
+        // fill: "rgba(0,255,0,0.1)",
+      },
+      {
+        stroke: "blue",
+        label: "AcZ",
+        // fill: "rgba(0,0,255,0.1)",
+      },
+    ],
+    axes: [
+      {},
+      {
+        label: "MPU read",
+        size: 60
+      },
+    ]
+  }
+}
